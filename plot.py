11602f5aab8619208cb93daccf34652749bf48fd
Sam Hendrickx <samhendrickx@gmail.com>
2016-05-02 20:56:53 +0200

    Final

--- plot.py
+++ plot.py
@@ -1,13 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 plt.ion()
-
-with open('data.txt', 'r') as f:
-    value = f.readline()
-    print value
-
 i = 0
 maxY = 65536 # 2 ^ 16;
 
@@ -19,6 +13,6 @@ while True:
 	    	xMin = 0 if i < 100 else i - 100
 	    	plt.axis([xMin, 10+i, 0, maxY])
 	    	plt.scatter(i, value);
-	    	plt.pause(0.00001)
+	    	plt.pause(0.0001)
 	    	i += 1
 
