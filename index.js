cd25f89f414b281d4d4285ff1561ce083516e33f
Sam Hendrickx <samhendrickx@gmail.com>
2016-05-03 11:29:14 +0200

    Gyroscope

--- index.js
+++ index.js
@@ -36,9 +36,6 @@ noble.on('discover', function(peripheral) {
 
       angelSensor.discoverServices(null, function(error, services) {
         for (var i in services) {
-          console.log(services[i]);
-
-          /*
           var service = services[i];
           var serviceUuid = service.uuid;
           switch(serviceUuid) {
@@ -64,7 +61,6 @@ noble.on('discover', function(peripheral) {
               activityMonitoringService(service);
               break;
           }
-          */
         }
 
       });
